642382b797726f2a10ef7e480e8c0ea334719fdf
Mohamed A. Abd El-Nabey <51803872+MOo-Ahmed@users.noreply.github.com>
2020-12-31 13:40:45 +0200

    Finished requirement 1.5

--- src/java/processFilteredSearch.java
+++ src/java/processFilteredSearch.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -14,10 +13,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/processSearch"})
-public class processSearch extends HttpServlet {
 
-    private String getAllHotels(int _rooms, int _adults, int _children, String checkIn, String checkOut, String city) 
+@WebServlet(urlPatterns = {"/processFilteredSearch"})
+public class processFilteredSearch extends HttpServlet {
+
+    private String getAllHotels(String opName, int opValue ,  int _rooms, int _adults, int _children, String checkIn, String checkOut, String city) 
             throws IOException, SQLException, ClassNotFoundException{
         city = city.toLowerCase();
         String output = "" ;
@@ -27,16 +27,34 @@ public class processSearch extends HttpServlet {
         Connection Con = null;
         Class.forName("com.mysql.jdbc.Driver");
         Con = DriverManager.getConnection(url, user, password);
-        String line = "SELECT DISTINCT (hotel.id) AS HotelID, COUNT(hotel.id) AS RoomsCount , "
+        String filterCondition = "" ;
+        if(opName.compareTo("price") == 0){
+            filterCondition = " HAVING exPrice <=" ;
+        }
+        else if(opName.compareTo("rate") == 0){
+            filterCondition = " HAVING RATE >=" ;
+        }
+        else if(opName.compareTo("stars") == 0){
+            filterCondition = " HAVING HotelStars >=" ;
+        }
+        else if(opName.compareTo("distance") == 0){
+            filterCondition = " HAVING Dist <=" ;
+        }
+        else if(opName.compareTo("meals") == 0){
+            filterCondition = " HAVING Meals = " ;
+        }
+        // For each case you may eant to make a separate query line
+        String line = "SELECT DISTINCT (hotel.id) AS HotelID, COUNT(hotel.id) AS RoomsCount , hotel.distanceFromCC AS Dist, hotel.includingMeals AS Meals,"
                 + " AVG(room.price) AS exPrice, AVG (review.rate) AS RATE, hotel.name AS HotelName, hotel.stars AS HotelStars "
                 + "FROM hotel INNER JOIN room "
                 + " ON hotel.id = room.hotelID AND hotel.cityID IN (SELECT city.id FROM city WHERE LOWER(city.name) LIKE ?) "
-                + " INNER JOIN review ON review.hotelID = hotel.id GROUP BY (hotel.id)";
-        PreparedStatement statement = Con.prepareStatement(line);
-        //statement.setString(1, checkIn);
-        statement.setString(1, city);
-        //statement.setString(3, _rooms + "");
+                + " INNER JOIN review ON review.hotelID = hotel.id GROUP BY (hotel.id) " + filterCondition + " ?";
         
+        PreparedStatement statement = Con.prepareStatement(line);
+        statement.setString(1, city);
+        if(opName.compareTo("meals") == 0)   
+                statement.setString(2, "Yes");
+        else    statement.setString(2, opValue + ""); 
         ResultSet RS = statement.executeQuery();
         boolean empty = true ;
         while (RS.next()) {
@@ -44,9 +62,12 @@ public class processSearch extends HttpServlet {
             int id = RS.getInt("HotelID") ;
             String name = RS.getString("HotelName") ;
             int exPrice = (int)Math.ceil(RS.getDouble("exPrice"));
-            int stars = RS.getInt("HotelStars");
             double rate = RS.getDouble("RATE");
-            output += makeDiv(id, name, exPrice, rate, stars) ;
+            int stars = RS.getInt("HotelStars") ;
+            double distance = RS.getDouble("Dist");
+            String meals = RS.getString("Meals");
+            if(meals.compareTo("Yes") == 0) meals = "Including meals" ;
+            output += makeDiv(id, name, exPrice, rate, stars, distance, meals) ;
         }
         if(empty)   {
             output = "<h3>Sorry, no result matches your search</h3>" ;        
@@ -55,72 +76,47 @@ public class processSearch extends HttpServlet {
         return output;
     }
     
-    private String makeDiv(int hotelID, String hotelName, int exPrice, double rate, int stars){
+    private String makeDiv(int hotelID, String hotelName, int exPrice, double rate, int stars, double distance, String meals){
         String div = "<div><h3>" + hotelName + "  -  " + exPrice +  " Pounds per night" + 
                 "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-                + " Rate : " + rate + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  " + stars + " STARS</h3><br>" ;
+                + " Rate : " + rate + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  " + stars + " STARS" +
+                "&nbsp;&nbsp;&nbsp;" + distance + " KM from city center" + "&nbsp;&nbsp;&nbsp; - " + meals + "</h3><br>" ;
         div += "<form class='form Log-form' action='HotelProfile.jsp'><input name = 'hotelID' type='hidden' value=" + hotelID + ">" ;
         div += "<img id ='si' src = 'img/h" + hotelID + ".jpg'>" ;
         div += "<br><br><input type='submit' value='View Hotel' class='btn form-btn btn-purple'>" ;
         div += "</form></div><br><br><br>" ;
         return div ;
     }
-    
-    private String getFilteringForm(String city, int rooms, String checkInDate, String checkOutDate){
-        String form = "<form class='form Log-form'  action='processFilteredSearch'>" +
-                "<h3 style='color:white;'>Filter search results using : </h3><br><br>";
-        form += "<input type='radio' id='price' name='options' value='price'> " 
-        + "<label for='price'>Price</label><br><br>" + 
-        "<input type='radio' id='stars' name='options' value='stars'> " 
-        + "<label for='stars'>Hotel Stars</label><br><br>" + 
-        "<input type='radio' id='rate' name='options' value='rate'> " +
-                "<label for='meals'>Rating</label><br><br>" + 
-                "<input type='radio' id='meals' name='options' value='meals'> " +
-                "<label for='distance'>Including meals</label><br><br>" + 
-                "<input type='radio' id='distance' name='options' value='distance'> " 
-        + "<label for='rate'>Distance from city center</label><br><br>" ;
-        form += "<input  class='input' type = 'number' name = 'val' > " ;
-        form += "<input type = 'hidden' name = 'city' value = " + city + " > " ;
-        form += "<input type = 'hidden' name = 'rooms' value = " + rooms + " > " ;
-        form += "<input type = 'hidden' name = 'checkInDate' value = " + checkInDate + " > " ;
-        form += "<input type = 'hidden' name = 'checkOutDate' value = " + checkOutDate + " > " ;
-        form += "<input type='submit' value = 'Go' class='btn form-btn btn-purple'></form><br><br><br><br><br>" ;
-        return form ;
-    }
+
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException, ClassNotFoundException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /*
-             1. In the previous page, the user entered the num of rooms, adults and children, the dates of check in and out 
-             2. Get all hotels having number of rooms >= rooms required as long as these rooms are not
-             reserved in the interval he entered and can accomodate children if any
-             3. Return a result set array / JSON
-             4. Loop through the result set and add a div for each hotel
             
-             */
+            String optionName = request.getParameter("options");
+            int optionValue = 0;
+            if(optionName.compareTo("meals") != 0)      optionValue = Integer.parseInt(request.getParameter("val"));
             String city = request.getParameter("city");
             int rooms = Integer.parseInt(request.getParameter("rooms"));
             String checkInDate = request.getParameter("checkInDate");
             String checkOutDate = request.getParameter("checkOutDate");
-            int adults = Integer.parseInt(request.getParameter("adults"));
-            int children = Integer.parseInt(request.getParameter("children"));
-            String r = getAllHotels(rooms, adults, children, checkInDate, checkOutDate, city);
+            int adults = 0;
+            int children = 0;
+            
+            String r = getAllHotels(optionName, optionValue , rooms, adults, children, checkInDate, checkOutDate, city);
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Search results</title>");
+            out.println("<title>Filteration results </title>");
             out.println("<link rel='stylesheet' href='styles.css'>");
             out.println("</head>");
             out.println("<body>");
             out.println("<main>");
+            out.println("<br><br><a href='customerHome.jsp' class='btn form-btn btn-purple'>Back To Search page</a>");
             
             out.println("<section class='Log'>");
             out.println("<div class='container'>");
-            out.println(getFilteringForm(city, rooms, checkInDate, checkOutDate));
-
-                
             out.println(r);
             out.println("</div>");
             out.println("</section>");
@@ -129,36 +125,55 @@ public class processSearch extends HttpServlet {
             out.println("</html>");
         }
     }
-
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
             processRequest(request, response);
         } catch (SQLException ex) {
-            Logger.getLogger(processSearch.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(processFilteredSearch.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(processSearch.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(processFilteredSearch.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
             processRequest(request, response);
         } catch (SQLException ex) {
-            Logger.getLogger(processSearch.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(processFilteredSearch.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(processSearch.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(processFilteredSearch.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-
 
 }
---
+++ web/HotelProfile.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : HotelProfile
+    Created on : Dec 30, 2020, 11:07:50 PM
+    Author     : m
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
