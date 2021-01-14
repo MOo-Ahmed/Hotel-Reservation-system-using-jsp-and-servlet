03fd89fa6ddc8306ef78c6fa786fbb6e8500de41
Mohamed A. Abd El-Nabey <51803872+MOo-Ahmed@users.noreply.github.com>
2021-01-15 01:05:41 +0200

    added admin info update form

--- src/java/adminHome.java
+++ src/java/adminHome.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -56,8 +57,10 @@ public class adminHome extends HttpServlet {
         return div;
     }
 
-    private String getAdminName(String AdminID)
+    private ArrayList<String> getAdminInfo(String AdminID)
             throws IOException, SQLException, ClassNotFoundException {
+        //Returns : name,username,password,email,phone
+        ArrayList<String> info = new ArrayList<>();
         String output = "";
         String url = "jdbc:mysql://localhost:3306/hotelreservationsystem";
         String user = "root";
@@ -65,7 +68,7 @@ public class adminHome extends HttpServlet {
         Connection Con = null;
         Class.forName("com.mysql.jdbc.Driver");
         Con = DriverManager.getConnection(url, user, password);
-        String line = "SELECT user.username AS Name FROM user WHERE user.id = ?";
+        String line = "SELECT * FROM user WHERE user.id = ?";
         PreparedStatement statement = Con.prepareStatement(line);
         statement.setString(1, AdminID);
 
@@ -73,27 +76,63 @@ public class adminHome extends HttpServlet {
         boolean empty = true;
         while (RS.next()) {
             empty = false;
-            output = RS.getString("Name");
+            info.add(RS.getString("name"));
+            info.add(RS.getString("username"));
+            info.add(RS.getString("password"));
+            info.add(RS.getString("email"));
+            info.add(RS.getString("phoneNumber"));
+            
         }
 
         Con.close();
-        return output;
+        return info;
     }
 
+    private String getUserInfoUpdateForm(String userID, String username , String  name, String  Password , String  phone, String email){
+        String output = "<br><br><br><br> "
+                + "<h5 class='section-head'>"
+                + "<span class='heading'>View & Update your personal info</span>"
+                + "<span class='sub-heading'>Not allowed to change the username</span></h5>"
+                + "<div class='Log-content'>"
+                + "<form action='processUpdateUserInfo' id='personalInfoForm' method='POST' class='form Log-form'>"
+                + "<label>Username : " + username + " (Unchangeable)</label><br><br>"
+                + "<input id='source' class='input' type = 'hidden' name='source' value='admin'>"
+                + "<input name='id' id='id' class='input' type = 'hidden' value=" + userID +">"
+                + "<label>Name : </label> <input id='name' class='input' type = 'text' name= 'name' value = '" + name + "'>"
+                + "<label>Password : </label><input id='password' class='input' type = 'text' name='password' placeholder = " + Password + " value = " + Password + " required>"
+                + "<label>Phone : </label><input id='phone' class='input' type = 'number' name='phone' placeholder = " + phone + " value = " + phone + " required>"
+                + "<label>Email : </label><input id='email' class='input' type = 'email' name='email' placeholder = '" + email + "' value = '" + email + "' required>"
+                + "<input id='submitChangeInfo' type='submit' value = 'Update my info' "
+                + "class='btn form-btn btn-purple'>"
+                + "<br><br><br>"
+              
+                + "</form>"
+                + " </div>" ;
+        return output ;
+    }
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException, ClassNotFoundException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            HttpSession session = request.getSession(true);
+            HttpSession session = request.getSession(true)  ;
             if (session.getAttribute("adminID") == null || session.getAttribute("adminID").toString().equals("")) {
                 response.sendRedirect("Login.jsp");
             }
             
+            //session = request.getSession(true);
             String adminID = session.getAttribute("adminID").toString();
-
+            ArrayList<String> info = getAdminInfo(adminID);
             
             String r = getAllHotels(adminID);
-            String username = getAdminName(adminID);
+            String name = info.get(0);
+            String username = info.get(1);
+            String Password = info.get(2);
+            String email = info.get(3);
+            String phone = info.get(4);
+            
+            String form = getUserInfoUpdateForm(adminID, username, name, Password, phone, email) ;
+            
 
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -113,6 +152,7 @@ public class adminHome extends HttpServlet {
                     + "<span class='dots'><i class='fas fa-ellipsis-h'></i></span></a><br><br><br>");
 
             out.println(r);
+            out.println(form);
             out.println("</div>");
             out.println("</section>");
             out.println("</main>");
